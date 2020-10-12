eec1dd2e330feb3d030b025a752c3f43fd937e5a
Bastian Nazaromi <bastian.nazaromi@gmail.com>
2020-10-13 05:37:18 +0700

    Commit pertama

---
+++ application/modules/daftar/views/v_daftar.php
@@ -0,0 +1,82 @@
+<div class="container">
+    <div class="row">
+        <div class="col-xl-12">
+            <div class="header">
+                Form Pendaftaran
+            </div>
+            <div class="panel panel-default">
+                <div class="panel-heading">A. Biodata Calon Mahasiswa</div>
+                <div class="panel-body">
+                    <div class="form-portlet donation-form-outer" style="background-color: white;">
+                        <div class="row clearfix">
+
+                            <div class="form-group col-lg-6 col-md-6 col-xs-12">
+                                <div class="field-label">NIK <span class="required">*</span></div>
+                                <input type="text" required placeholder="NIK" name="nik">
+                            </div>
+
+                            <div class="form-group col-lg-6 col-md-6 col-xs-12">
+                                <div class="field-label">Nama <span class="required">*</span></div>
+                                <input type="text" required placeholder="Nama lengkao" name="nama">
+                            </div>
+
+                            <div class="form-group col-lg-6 col-md-6 col-xs-12">
+                                <div class="field-label">Tempat / Tanggal Lahir <span class="required">*</span></div>
+                                <input type="text" required placeholder="Tanggal lahir" name="ttl">
+                            </div>
+
+                            <div class="form-group col-lg-6 col-md-6 col-xs-12">
+                                <div class="field-label">Agama <span class="required">*</span></div>
+                                <select>
+                                    <option>Islam</option>
+                                    <option>Kristen Protestan</option>
+                                    <option>Kristen Katolik</option>
+                                    <option>Hindu</option>
+                                    <option>Buddha</option>
+                                    <option>Konghuchu </option>
+                                </select>
+                            </div>
+
+                            <div class="form-group col-lg-6 col-md-6 col-xs-12">
+                                <div class="field-label">Jenis Kelamin <span class="required">*</span></div>
+                                <select>
+                                    <option>Laki - laki</option>
+                                    <option>Perempuan</option>
+                                </select>
+                            </div>
+
+                            <div class="form-group col-lg-6 col-md-6 col-xs-12">
+                                <div class="field-label">Status Mahasiswa <span class="required">*</span></div>
+                                <select>
+                                    <option>Lajang</option>
+                                    <option>Menikah</option>
+                                </select>
+                            </div>
+
+                            <div class="form-group col-lg-6 col-md-6 col-xs-12">
+                                <div class="field-label">City <span class="required">*</span></div>
+                                <select>
+                                    <option>City</option>
+                                    <option>City Name</option>
+                                    <option>City Other</option>
+                                </select>
+                            </div>
+
+                            <div class="form-group col-lg-6 col-md-6 col-xs-12">
+                                <div class="field-label">Zip / Postal Code <span class="required">*</span></div>
+                                <input type="text" required="" placeholder="Zip Code" value="" name="name">
+                            </div>
+
+                            <hr>
+
+                            <div class="text-center"><button class="thm-btn mt_30 mb_30" type="submit">Submit
+                                    Now</button></div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
---
+++ application/modules/daftar/views/v_home.php
@@ -0,0 +1,39 @@
+<section class="rev_slider_wrapper">
+    <div class="row">
+        <div class="col-lg-12">
+            <div id="slider1" class="rev_slider" data-version="5.0">
+                <ul>
+                    <li data-transition="parallaxvertical">
+                        <div class="tp-caption sfl tp-resizeme thm-banner-h1" data-x="left" data-hoffset="0"
+                            data-y="top" data-voffset="225" data-whitespace="nowrap" data-transform_idle="o:1;"
+                            data-transform_in="o:0" data-transform_out="o:0" data-start="500">
+                            POLITKNIK HARAPAN BERSAMA
+                        </div>
+                        <div class="tp-caption sfr tp-resizeme thm-banner-h1 heavy" data-x="left" data-hoffset="0"
+                            data-y="top" data-voffset="290" data-whitespace="nowrap" data-transform_idle="o:1;"
+                            data-transform_in="o:0" data-transform_out="o:0" data-start="1000">
+                            THE TRUE VOCATIONAL CAMPUS
+                        </div>
+                        <div class="tp-caption sfb tp-resizeme thm-banner-h3" data-x="left" data-hoffset="0"
+                            data-y="top" data-voffset="368" data-whitespace="nowrap" data-transform_idle="o:1;"
+                            data-transform_in="o:0" data-transform_out="o:0" data-start="1500">
+                            Orang Cerdas Kuliah di PHB
+                        </div>
+
+                        <div class="tp-caption sfl tp-resizeme" data-x="left" data-hoffset="0" data-y="top"
+                            data-voffset="505" data-whitespace="nowrap" data-transform_idle="o:1;"
+                            data-transform_in="o:0" data-transform_out="o:0" data-start="2300">
+                            <a href="#" class="thm-btn">Mulai Daftar</a>
+                        </div>
+                        <div class="tp-caption sfr tp-resizeme" data-x="left" data-hoffset="185" data-y="top"
+                            data-voffset="505" data-whitespace="nowrap" data-transform_idle="o:1;"
+                            data-transform_in="o:0" data-transform_out="o:0" data-start="2600">
+                            <a href="#" class="thm-btn inverse">Pengumuman</a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+</section>
